39348dae6fa5effd882714aefa883ffb6a132ab8
Heza GÖLCÜK <h.golcuk91@outlook.com>
2024-07-02 16:46:11 +0300

    js-1 homework finished

---
+++ js/task-3.js
@@ -0,0 +1,12 @@
+function getElementWidth(content, padding, border) {
+    let contentWidth = parseFloat (content);
+    let paddingWidth = parseFloat (padding);
+    let borderWidth = parseFloat (border);
+    let totalWidth = (borderWidth + paddingWidth) * 2 + contentWidth;
+    return totalWidth;
+}
+
+
+console.log(getElementWidth("50px", "8px", "4px")); 
+console.log(getElementWidth("60px", "12px", "8.5px")); 
+console.log(getElementWidth("200px", "0px", "0px")); 
